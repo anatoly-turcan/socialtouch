ed0e8de57f35bdbe98eaac78dc1ba675fba3cc86
4natoli.t <4natoli.t@gmail.com>
2020-07-07 22:12:17 +0300

    Signout implementation

--- api/routes/authRoutes.js
+++ api/routes/authRoutes.js
@@ -5,6 +5,7 @@ const router = Router();
 
 router.route('/signup').post(authController.signup);
 router.route('/signin').post(authController.signin);
+router.route('/signout').post(authController.signout);
 router.route('/forgotPassword').post(authController.forgotPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
 
--- client/src/App.js
+++ client/src/App.js
@@ -5,11 +5,11 @@ import Signin from './components/auth/signin';
 import Signup from './components/auth/signup';
 import Forgot from './components/auth/forgot';
 import Restore from './components/auth/restore';
+import Signout from './components/auth/signout';
 import Navbar from './components/navbar';
 import UserContext from './context/userContext';
-
-import './App.css';
 import UserPage from './pages/user';
+import './App.css';
 
 const App = () => {
   const [user, setUser] = useState(JSON.parse(localStorage.getItem('user')));
@@ -26,6 +26,7 @@ const App = () => {
             {!user && <Route path="/restore" component={Restore} />}
             {!user && <Redirect to="/signin" />}
 
+            <Route path="/signout" component={Signout} />
             <Route path="/not-found" component={NotFound} />
             <Route path="/:link" component={UserPage} />
             <Redirect to="/not-found" />

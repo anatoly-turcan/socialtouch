61d35be282ba593b6e78c4ceec27308630d86d21
4natoli.t <4natoli.t@gmail.com>
2020-07-09 19:25:01 +0300

    Fix load more posts

--- client/src/components/common/posts.jsx
+++ client/src/components/common/posts.jsx
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PostBox from './postBox';
 
-const Posts = ({ posts, handleLoadMore }) => {
+const Posts = ({ fetchMethod }) => {
+  const [posts, setPosts] = useState([]);
+  const [page, setPage] = useState(1);
+  const [loader, setLoader] = useState(true);
+  const [isAll, setIsAll] = useState(false);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoader(true);
+      const newPosts = await fetchMethod(page);
+      if (!newPosts.length) setIsAll(true);
+      else setPosts([...posts, ...newPosts]);
+      setLoader(false);
+    };
+    fetchPosts();
+  }, [page]);
+
+  const handleLoadMore = (event) => {
+    event.preventDefault();
+    setPage(page + 1);
+  };
+
   return (
     <div className="posts">
       {posts.length ? (
@@ -9,13 +30,20 @@ const Posts = ({ posts, handleLoadMore }) => {
       ) : (
         <div className="centered-info">No posts</div>
       )}
-      <div className="post__box">
-        {posts.length > 0 && posts.length % 10 === 0 && (
+
+      {loader && (
+        <div className="post__box">
+          <div className="post__box--load-more centered">Loading...</div>
+        </div>
+      )}
+
+      {!loader && !isAll && posts.length > 0 && posts.length % 10 === 0 && (
+        <div className="post__box">
           <button className="post__box--load-more" onClick={handleLoadMore}>
             Load more
           </button>
-        )}
-      </div>
+        </div>
+      )}
     </div>
   );
 };
--- client/src/pages/user.jsx
+++ client/src/pages/user.jsx
@@ -3,7 +3,7 @@ import UserContext from '../context/userContext';
 import api from '../services/apiService';
 import ProfileBox from '../components/profileBox';
 import Posts from '../components/common/posts';
-import { useParams, Link } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 
 const User = ({ history }) => {
   const params = useParams();
@@ -11,8 +11,6 @@ const User = ({ history }) => {
   const [link, setLink] = useState(params.link);
   const [linkedUser, setLinkedUser] = useState(null);
   const [friends, setFriends] = useState([]);
-  const [page, setPage] = useState(1);
-  const [posts, setPosts] = useState([]);
   const [loader, setLoader] = useState(false);
 
   useEffect(() => {
@@ -23,7 +21,6 @@ const User = ({ history }) => {
   }, [params]);
 
   useEffect(() => {
-    setPage(1);
     const fetchUser = async () => {
       try {
         setLinkedUser(await api.getUser(link));
@@ -40,32 +37,23 @@ const User = ({ history }) => {
     if (linkedUser) {
       const fetchFriendsPosts = async () => {
         setFriends(await api.getFriends(linkedUser.link));
-        setPosts(await api.getPosts(linkedUser.link, page));
         setLoader(false);
       };
       fetchFriendsPosts();
     }
   }, [linkedUser]);
 
-  useEffect(() => {
-    if (linkedUser) {
-      const fetchPosts = async () => {
-        setPosts([...posts, ...(await api.getPosts(linkedUser.link, page))]);
-      };
-      fetchPosts();
-    }
-  }, [page]);
-
-  const handleLoadMore = (event) => {
-    event.preventDefault();
-    setPage(page + 1);
-  };
-
   if (loader) return <div className="global-loader">Loading...</div>;
 
   return (
     <div className="content__personal-page">
-      <Posts posts={posts} handleLoadMore={handleLoadMore} />
+      {linkedUser && (
+        <Posts
+          fetchMethod={async (page) =>
+            await api.getPosts(linkedUser.link, page)
+          }
+        />
+      )}
       {linkedUser && <ProfileBox user={linkedUser} friends={friends} />}
     </div>
   );

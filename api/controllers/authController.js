0a3740ca183a42c12bbdafba6f7d71dcf3354625
4natoli.t <4natoli.t@gmail.com>
2020-07-30 23:47:56 +0300

    Cookie expires fix

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -33,7 +33,7 @@ const createSendToken = (user, statusCode, res) => {
   res.cookie('jwt', token, {
     ...cookieOptions,
     expires: new Date(
-      Date.now() + process.env.JWTCOOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
   });
 
